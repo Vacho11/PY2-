98afa6eecd09553a7136488cd73c703a5e6c62b3
Valeri Mdivani <Mdivanivacho@gmail.com>
2017-01-20 18:32:54 +0300

    Updated code for homework 2.1

--- cook.py
+++ cook.py
@@ -1,8 +1,12 @@
+cook_book = {}
+
 with open('cook_book.txt') as cook_book_file:
 	for line in cook_book_file:
-		Dish_name = line.strip()
-		ingridients_line_count = int(line.readline())
-
+		if not line.isspace():
+			dish = {}
+			Dish_name = line.strip()
+		        ingridients_line_count = int(line.readline())
+		        dish['name'] = dish_name
 		Dish_ingredients = ""
 		for i in range(ingridients_line_count):
 			Dish_ingredients += " " + cook_book_file.readline().strip()
@@ -11,25 +15,25 @@ def get_shop_list_by_dishes(dishes, people_count):
     shop_list = {}
     for dish in dishes:
         for ingridient in dish['ingridients']:
-            new_shop_item = dict(ingridient)
+                new_shop_item = dict(ingridient)
             # пересчитали ингрединты по количеству людей
-            new_shop_item['quantity'] = new_shop_item['quantity'] * people_count
-            if new_shop_item['product'] not in shop_list:
+                new_shop_item['quantity'] = new_shop_item['quantity'] * people_count
+                if new_shop_item['product'] not in shop_list:
                 shop_list[new_shop_item['product']] = new_shop_item
-            else:
+                else:
                 shop_list[new_shop_item['product']]['quantity'] += new_shop_item['quantity']
-    return shop_list
+        return shop_list
 
 def print_shop_list(shop_list):
-    for key, shop_list_item in shop_list.items():
+        for key, shop_list_item in shop_list.items():
         print("{product} {quantity} {unit}".format(**shop_list_item))
 
 def create_shop_list(people_count, first_dish, second_dish, third_dish):
     # получить блюда из кулинарной книги
     dish1 = cook_book[first_dish]
-    dish2 = cook_book[second_dish]
-    dish3 = cook_book[third_dish]
-    dishes = [dish1, dish2, dish3]
+        dish2 = cook_book[second_dish]
+        dish3 = cook_book[third_dish]
+        dishes = [dish1, dish2, dish3]
     #заполнили список покупок
     shop_list = get_shop_list_by_dishes(dishes, people_count)
     # Вывести список покупок
